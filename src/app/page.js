99cf50789f304d697198f7addfcfba7f10d173f0
Gavin Raine Dizon <vcommsnetlify@gmail.com>
2024-12-29 09:29:40 +0800

    feature: integrate all sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,78 @@
+import Footer from "@/components/Footer";
+import Navigation from "@/components/Navigation";
+import FAQ from "@/sections/FAQ";
 import Image from "next/image";
+import Link from "next/link";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillInstagram, AiFillTwitterCircle } from "react-icons/ai";
+
+import { FaInstagram } from "react-icons/fa";
+import VideoJS from "@/components/VideoJS";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <div>
+            <Navigation />
+            <main className="bg-[url('/MLSTBG.png')] bg-no-repeat bg-cover w-full min-h-screen pt-0 md:pt-8 pb-8">
+                <section className="mx-auto container py-0 md:py-8">
+                    <iframe
+                        id="interprefyiFrame"
+                        src="https://interprefy.interpret.world/loginlink?token=MLST2025&caption=true"
+                        className="w-full h-[80vh]"
+                    ></iframe>
+                </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                <section id="contact-us" className="mx-auto container py-16">
+                    <div className="w-4/5 md:w-full h-32 md:h-48 relative mx-auto">
+                        <Image
+                            src={"/mylifestorytime.png"}
+                            alt="My Life Story Time"
+                            fill={true}
+                            className="object-contain"
+                        />
+                    </div>
+                    <p className="px-4 md:px-0 py-4 md:py-8 text-lg md:text-3xl max-w-prose mx-auto text-center leading-normal md:leading-loose">
+                        {" "}
+                        Every journey begins with a decision—it all starts here.
+                        <br />
+                        <span className="text-[#d0f600] font-bold">My Life My Story My Time </span>
+                        is about the courage to embrace who you are, take ownership of your story, and create your
+                        perfect moment instead of waiting for it.
+                    </p>
+                    <h3 className="text-xl md:text-4xl text-[#00fbae] text-center font-bold">
+                        Your time is now, what are you waiting for?
+                    </h3>
+                    <h4 className="text-center font-bold mt-8 text-xl md:text-3xl">Follow us and tag us on social:</h4>
+
+                    <div className="flex items-center justify-center space-x-4 md:space-x-8 py-4 flex-wrap">
+                        <div className="flex  items-center justify-center space-x-2">
+                            <FaFacebook className="text-lg md:text-2xl" />{" "}
+                            <Link
+                                href="https://www.facebook.com/@thevofficial"
+                                className="underline italic text-base md:text-xl"
+                                target="_blank"
+                            >
+                                @thevofficial
+                            </Link>
+                        </div>
+                        <div className="flex items-center justify-center space-x-2">
+                            <AiFillTwitterCircle className="text-xl md:text-3xl" />{" "}
+                            <AiFillInstagram className="text-xl md:text-3xl" />{" "}
+                            <Link
+                                href="https://www.facebook.com/@thevofficial"
+                                className="underline italic text-base md:text-xl"
+                                target="_blank"
+                            >
+                                @thev_official
+                            </Link>
+                        </div>
+                    </div>
+                    <h2 className="font-bold text-center text-xl sm:text-2xl md:text-4xl py-4">#MyLifeMyStoryMyTime</h2>
+                </section>
+
+                <FAQ />
+            </main>
+            <Footer />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
