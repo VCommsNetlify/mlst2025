ebf4c90747c44307c42c0b2ffcbb6a0dfa3d538a
Gavin Raine Dizon <vcommsnetlify@gmail.com>
2025-01-03 14:54:04 +0800

    add gtm (2)

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,13 +10,10 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={` antialiased`}>
-                <Script id="google-tag-manager" strategy="beforeInteractive">{`
-         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','G-034XRGX66F');
-        `}</Script>
+                <Script
+                    id="google-tag-manager"
+                    strategy="beforeInteractive"
+                >{`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-034XRGX66F');`}</Script>
                 {children}
                 <Script
                     src="https://players.brightcove.net/3745659807001/hXGs3fyvO_default/index.min.js"
