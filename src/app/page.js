64f43fcf75feedd546538d26b4cf31907fe03649
VCommsNetlify <167278407+VCommsNetlify@users.noreply.github.com>
2025-01-31 00:19:38 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,9 @@ export default function Home() {
         <div>
         
             <Navigation />
-                    <div className="w-full md:w-full h-80 md:h-80 relative mx-auto">
+                    
+            <main className="bg-main bg-no-repeat bg-cover w-full min-h-screen pt-0 md:pt-0 pb-0">
+                <div className="w-full md:w-full h-80 md:h-80 relative mx-auto">
                         <Image
                             src={"/VBP-logo.png"}
                             alt="My Life Story Time"
@@ -27,9 +29,6 @@ export default function Home() {
                             className="object-contain"
                         />
                     </div>
-            <main className="bg-main bg-no-repeat bg-cover w-full min-h-screen pt-0 md:pt-8 pb-8">
-                
-                    
                                 
                 <section className="w-full md:w-4/5 lg:w-3/5 mx-auto container py-0 md:py-4">
                     <div className="">
