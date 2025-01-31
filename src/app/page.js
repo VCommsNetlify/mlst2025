18d4f1ae942f8d8dac4f846b4f142bd71bd17dd7
VCommsNetlify <167278407+VCommsNetlify@users.noreply.github.com>
2025-01-31 14:35:23 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
                             className="object-contain"
                         />
                     </div>
-                    <div className="hidden md:block w-20 md:w-full h-20 md:h-20 relative mx-auto ">
+                    <div className="hidden lg:block w-20 md:w-full h-20 md:h-20 relative mx-auto ">
                         <Image
                             src={"/VBP-logo.png"}
                             alt="My Life Story Time"
