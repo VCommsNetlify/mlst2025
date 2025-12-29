1d161daa3e47e3db2335f95c1d7f524c22fde5ce
VCommsNetlify <167278407+VCommsNetlify@users.noreply.github.com>
2025-12-29 13:22:30 +0800

    adjustment

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,14 +3,19 @@ import React from "react";
 
 const Navigation = () => {
     return (
-        <header className=" w-full h-24  py-4  bg-white">
-            <nav className="px-4 md:px-0 mx-auto container flex items-center justify-start">
-                <div className="relative h-16 w-16 md:w-20">
-                    <Image src={"/vtube.png"} alt="VConnect Logo" fill={true} className="object-contain" />
-                </div>
-            </nav>
-        </header>
-    );
+        <header className="w-full h-16 bg-[#bbc9ff] flex items-center">
+            <nav className="px-4 md:px-0 mx-auto container flex items-center justify-end">
+                <div className="relative h-10 w-10 md:h-12 md:w-12">
+          <Image 
+            src={"/vtube.png"} 
+            alt="VConnect Logo" 
+            fill={true} 
+            className="object-contain" 
+          />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navigation;
